b5e36347060346d7d204842e13585d5476da2f37
Rosh0408 <roshg2001@gmail.com>
2024-10-12 16:14:06 +0100

    Add codes for practical3

---
+++ practical3/pi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+    float m_pi=M_PI;
+    float acos_pi=acos(-1);
+
+    printf("m_pi :%.16f\n", m_pi);
+    printf("acos_pi = %.16f\n",acos_pi);
+
+    return(0);
+
+
+}
